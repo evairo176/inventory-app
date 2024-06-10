0e1d51f8db533af6765e5d6be4b261cc518a3820
Dicki Prasetya <dickiprasetya176@gmail.com>
2024-06-10 18:51:14 +0700

    feat: update

--- src/providers/tanstack-query-provider.tsx
+++ src/providers/tanstack-query-provider.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+      refetchOnReconnect: false,
+    },
+  },
+});
 
 type Props = {
   children: React.ReactNode;
